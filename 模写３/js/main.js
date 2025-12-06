965c605945ba543c74cc75f1a42ff4c188ab51ec
iwazaa16 <divzawa@gmail.com>
2025-12-07 01:34:43 +0900

    first commit

---
+++ 模写３/js/main.js
@@ -0,0 +1,15 @@
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+const mask = document.getElementById('mask');
+
+hamburger.addEventListener('click',()=> {
+  hamburger.classList.toggle('active');
+  nav.classList.toggle('active');
+  mask.classList.toggle('active');
+});
+
+mask.addEventListener('click',()=> {
+  hamburger.classList.remove('active');
+  nav.classList.remove('active');
+  mask.classList.remove('active');
+})
